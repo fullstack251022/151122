57981786e7d26ff705fe74b2feb8b965cd43e3f7
fullstack251022 <116830126+fullstack251022@users.noreply.github.com>
2022-11-15 21:30:51 +0200

    Create castingAndRandomNumber.java

---
+++ castingAndRandomNumber.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+//        int a =5;
+////        int x = 10;
+//        int max = Math.max(5, 10);
+//        System.out.println(max);
+
+
+//        int x = (int)5.12345;
+//        System.out.println(x);
+
+//        double x =5;
+//        char myString =5;
+//        int myInt = myString;
+
+//        System.out.println(myInt);
+        int min =-100;
+        int max =100;
+        int myRandom =(int)((Math.random() * (max - min)) + min);
+        System.out.println(myRandom);
+
+
+    }
+}
