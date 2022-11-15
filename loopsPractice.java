fd514503e880bb5f1e93c5909203ecd025d8e5cb
fullstack251022 <116830126+fullstack251022@users.noreply.github.com>
2022-11-15 21:30:22 +0200

    Create loopsPractice.java

---
+++ loopsPractice.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //print numbers from 1000 -> 900
+//        for (int i = 0; i < 5; i++) {
+//            for (int j = 0; j < 5 ; j++) {
+//                System.out.print('*');
+//            }
+//            System.out.println();
+//        }
+
+//        for (int i = 0; i < 10; i++) {
+//            System.out.println(i);
+//            if (i == 4) {
+//                break;
+//            }
+//        }
+
+        //ask user to guess the number
+        //if the number is 0 print "Good Job!" and stop asking the number
+        //keep asking to guess number
+        //use while(true){} and break
+
+//        Scanner scanner = new Scanner(System.in);
+
+//        while (true){
+//            System.out.println("Please guess number");
+//            int guessedNumber = scanner.nextInt();
+//            if (guessedNumber == 0){
+//                System.out.println("Good Job! you guessed right");
+//                break;
+//            }
+//        }
+
+
+//      You enter to the restaurant and waiter asks you to buy drinks
+//      as soon as your purchased drinks for more than 250 nis stop ordering.
+
+
+//        Scanner scanner = new Scanner(System.in);
+//        int sum = 0;
+//        while (true) { //here I am entered the restaurant
+//
+//            System.out.println("Please make your order");
+//
+//            sum += scanner.nextInt();
+//            System.out.println("Money spent " + sum);
+//            if (sum >= 150) {
+//                System.out.println("Here your friend take you from the restaurant");
+//                break;
+//            }
+//        }
+//        System.out.println("Overall you spent "+sum+" shekels");
+
+
+//        for (int i = 0; i < 10; i++) {
+//            if (i == 4) {
+//                continue; //start next iteration
+//            }
+//            System.out.println(i);
+//        }
+
+//      here: for (int i = 0; i <= 5; i++) {
+//            System.out.println("i "+i);
+//            for (int j = 0; j <= 5 ; j++) {
+//                System.out.println("j "+j);
+//                if(j>=2){
+//                    break here;
+//                }
+//            }
+//        }
+
+
+
+    }
+}
